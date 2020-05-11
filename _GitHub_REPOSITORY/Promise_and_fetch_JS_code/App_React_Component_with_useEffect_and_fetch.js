4bb0a0f1a23e34381d5b4d37a9b56e1b84b6c6af
AndrasGitServer <andras_pc@hotmail.com>
2020-05-11 17:12:15 -0400

    React App.js with a Component using useEffect and fetch()

---
+++ _GitHub_REPOSITORY/Promise_and_fetch_JS_code/App_React_Component_with_useEffect_and_fetch.js
@@ -0,0 +1,110 @@
+/*	2020 May 11 Monday
+
+	UseEffect is another hook that we can use in functional components.
+	We use useEffect in order to manage "side Effects" in our React project.
+
+	This method will execute directly after the component is rendered, 
+	and every time the component updates.
+
+	A common way useEffect is used is when making a call to an API (API call).
+
+	Fetch is simply a way to make a request in Javascript. 
+	
+	When we make a request to the Star Wars API, we will get a response,
+	and part of that response is a list of the people in the Star Wars API. 
+	
+	We will then use setPeople in order to keep track of the list of people.
+
+	Second Argument
+	***************
+	useEffect() takes an optional second argument: an array that contains different variables. 
+	This second argument is very powerful, 
+	because we can be more specific and tell useEffect exactly when we want it to run. 
+	
+	useEffect will always run on first render (when a component first loads).
+	*************************************************************************
+
+	It will also run when a variable in the second argument array changes. 
+	If we have an empty array in the second argument, it will never change. 
+	Therefore, it will only run on first render.
+
+		
+			Then we get a response but the response we get is not JSON 
+			but an object with a series of methods we can use 
+			depending on what we want to do with the information.
+
+			GET request is the default for fetch()
+
+*/
+
+
+import React , { useState , useEffect } from 'react';
+import './App.css';
+
+
+function App() {
+
+  return (
+
+    <div className="App">
+    
+    	<Example1 />
+
+    </div>
+
+  );
+}
+
+
+const Example1 = (props) => {
+
+	const [ state , setState ] = useState(0);
+
+  	useEffect( () => {
+
+		console.log('** In useEffect() only once because of 2nd argument in useEffect() **');
+
+		
+		fetch('https://swapi.dev/api/people/')		// call to the Star Wars API
+
+			.then( response => response.json() )	
+			// or is the same as:	.then( response => { return response.json() } )
+			// there will be a result property after the .json() call
+						
+			.then( response => { 
+				console.log( response.results );
+				setState( { people: response.results } );
+			 } );
+			
+	}, [] );
+
+
+  	return (
+
+    	<div>
+			<h2>
+
+				{
+					state.people ? 
+						state.people.map( (item,index) => {
+							return <div key={ index }> { item.name } </div>
+						})
+					: null 
+				}
+
+				<hr/>
+				
+				{ JSON.stringify( state.people ) }
+				
+						
+			</h2>
+		</div>
+
+  	);
+}
+
+
+export default App;
+
+	
+
